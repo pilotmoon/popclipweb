1eca3d00e7f7bc2bdc458e055d46fccd19c180f1
Nick Moore <nick@pilotmoon.com>
2023-08-26 13:42:56 +0100

    remove .ts extension from import

--- site/components/Setapp.vue
+++ site/components/Setapp.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { onMounted } from 'vue'
-import { loadScript } from './helpers/loadScript.ts'
+import { loadScript } from './helpers/loadScript'
 import * as config from './config/config.json'
 
 onMounted(() => {
