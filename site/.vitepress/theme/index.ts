9a488b97d6bae790433c3f9681b7dc602e77cc46
Nick Moore <nick@pilotmoon.com>
2023-08-30 22:02:27 +0100

    add vercel analytics

--- site/.vitepress/theme/index.ts
+++ site/.vitepress/theme/index.ts
@@ -3,12 +3,14 @@ import { h } from "vue";
 import Theme from "vitepress/theme";
 import "./style.css";
 import { GithubFilled } from '@ant-design/icons-vue';
+import Vercel from '../../src/Vercel.vue';
 
 export default {
   extends: Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'layout-bottom': () => h(Vercel)
     });
   },
   enhanceApp({ app, router, siteData }) {
