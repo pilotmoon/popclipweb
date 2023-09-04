1adeafd8429dae8e97a2446b3302b8ad1092a476
Nick Moore <nick@pilotmoon.com>
2023-09-04 12:28:41 +0100

    try remove vercel insertion

--- site/src/Vercel.vue
+++ site/src/Vercel.vue
@@ -2,19 +2,19 @@
 import { onMounted } from 'vue'
 
 onMounted(() => {
-    if (typeof window !== "undefined" && window.location.hostname === "localhost") {
-        return;
-    }
-    setTimeout(() => {
-        const script1 = document.createElement("script");
-        script1.textContent = "window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };"
-        document.head.appendChild(script1);
+    // if (typeof window !== "undefined" && window.location.hostname === "localhost") {
+    //     return;
+    // }
+    // setTimeout(() => {
+    //     const script1 = document.createElement("script");
+    //     script1.textContent = "window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };"
+    //     document.head.appendChild(script1);
 
-        const script2 = document.createElement("script");
-        script2.setAttribute("src", "/_vercel/insights/script.js");
-        script2.defer = true;
-        document.head.appendChild(script2);
-    }, 500);
+    //     const script2 = document.createElement("script");
+    //     script2.setAttribute("src", "/_vercel/insights/script.js");
+    //     script2.defer = true;
+    //     document.head.appendChild(script2);
+    // }, 500);
 
 });
 </script>
