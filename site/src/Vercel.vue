e87081d7e7479a52638e161a7069b1fe9ac208f9
Nick Moore <nick@pilotmoon.com>
2023-09-04 12:33:57 +0100

    Uncomment script

--- site/src/Vercel.vue
+++ site/src/Vercel.vue
@@ -2,23 +2,22 @@
 import { onMounted } from 'vue'
 
 onMounted(() => {
-    // if (typeof window !== "undefined" && window.location.hostname === "localhost") {
-    //     return;
-    // }
-    // setTimeout(() => {
-    //     const script1 = document.createElement("script");
-    //     script1.textContent = "window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };"
-    //     document.head.appendChild(script1);
+    if (typeof window !== "undefined" && window.location.hostname === "localhost") {
+        return;
+    }
+    
+    const script1 = document.createElement("script");
+    script1.textContent = "window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };"
+    document.head.appendChild(script1);
 
-    //     const script2 = document.createElement("script");
-    //     script2.setAttribute("src", "/_vercel/insights/script.js");
-    //     script2.defer = true;
-    //     document.head.appendChild(script2);
-    // }, 500);
-
+    const script2 = document.createElement("script");
+    script2.setAttribute("src", "/_vercel/insights/script.js");
+    script2.defer = true;
+    document.head.appendChild(script2);
 });
 </script>
 
 <template>
+    <!-- Must have some content, or we get hydration errors -->
     <div id="vercel-insights"></div>
 </template>
