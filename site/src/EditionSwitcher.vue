5f796fcc0f84457089d76e38add3b6887e5f2f7c
Nick Moore <nick@pilotmoon.com>
2023-09-04 10:53:22 +0100

    edition switcher now works on page refresh

--- site/src/EditionSwitcher.vue
+++ site/src/EditionSwitcher.vue
@@ -3,18 +3,27 @@ import Theme from './Theme.vue';
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'element-plus/es/components/switch/style/css'
 import './style/overrides.css'
-import { ElSwitch, ElSpace } from 'element-plus'
+import { ElSwitch } from 'element-plus'
 import { useEditionSwitcherState } from './state/useEditionSwitcherState'
-import { computed } from 'vue';
+import { WatchStopHandle, onBeforeUnmount, onMounted, ref, watch } from 'vue';
 
 const { edition } = useEditionSwitcherState();
-const state = computed({
-    get() {
-        return edition.value === 'setapp';
-    },
-    set(newValue) {
-        edition.value = newValue ? 'setapp' : 'base';
-    }
+const state = ref(false);
+const updateSwitch = (newEditionString) => {
+    state.value = newEditionString === 'setapp';
+}
+const stoppers: WatchStopHandle[] = [];
+onMounted(() => {
+    console.log('mounted edition', edition.value);
+    stoppers.push(watch(edition, updateSwitch));
+    stoppers.push( watch(state, (newVal) => {
+        edition.value = newVal ? 'setapp' : 'base';
+    }));
+    updateSwitch(edition.value);
+});
+onBeforeUnmount(() => {
+    console.log('stopping watchers');
+    stoppers.forEach((stop) => stop());
 });
 </script>
 
