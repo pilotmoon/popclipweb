a86aa3ffd3b2a67019a5bf35d92f613d338688c8
Nick Moore <nick@pilotmoon.com>
2023-09-04 14:09:27 +0100

    Add Download event trackine

--- site/src/DownloadButton.vue
+++ site/src/DownloadButton.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
+import va from '@vercel/analytics';
 import MyButton from './MyButton.vue';
 import { DownloadOutlined } from '@ant-design/icons-vue';
 const props = defineProps<{
   iconOnly?: boolean
+  href: string
 }>();
-
+const track = () => {
+  console.log(`Download ${props.href}`);
+  va.track("Download", {
+    url: props.href
+  });
+};
 </script>
 
 <template>
-    <MyButton>
+    <MyButton @click="track" :href="props.href">
         <DownloadOutlined /><span v-if="!iconOnly"> Download</span>
     </MyButton>
 </template>
