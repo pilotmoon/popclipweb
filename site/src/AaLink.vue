ebe3c1b8c036ac6727d42666f0ede75aaf17a0ef
Nick Moore <nick@pilotmoon.com>
2023-09-05 16:49:46 +0100

    Github icon tweaking

--- site/src/AaLink.vue
+++ site/src/AaLink.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import config from './config/config.json';
-import { computed, onMounted } from 'vue';
-import { useStoreState, loadStore } from './composables/useStoreState';
+import { computed } from 'vue';
+import { useStoreState } from './composables/useStoreState';
 import { GithubFilled } from '@ant-design/icons-vue';
 
 const store = useStoreState();
@@ -12,25 +12,25 @@ const props = defineProps<{
 }>()
 
 const ghTrimmed = computed(() => {
-    if(props.gh) {
+    if (props.gh) {
         return props.gh.replace(/^https?:\/\/github.com\//, '');
     }
     return '';
 });
 
 const ghDisplay = computed(() => {
-    if(props.gh) {
+    if (props.gh) {
         return ghTrimmed.value.replace(/tree\/master\//, '');
     }
     return '';
 });
 
 const href = computed(() => {
-    if(props.href) {
+    if (props.href) {
         return props.href;
     }
 
-    if(props.gh) {
+    if (props.gh) {
         return `https://github.com/${ghTrimmed.value}`;
     }
 
@@ -47,106 +47,29 @@ const href = computed(() => {
         }
         return typeof value === 'string' ? value : '';
     }
-    
-    return '';
-});
 
-onMounted(() => {
-    loadStore();
+    return '';
 });
 
 </script>
 
 <template>
-    <span v-if="props.gh"><GithubFilled /> <a :href="href"><span v-html="ghDisplay" /></a></span>
-    <a v-else :href="href"><slot /></a>
+    <span class="IconLink" v-if="props.gh">
+        <GithubFilled class="GithubIcon" />
+        <a :href="href"><span v-html="ghDisplay" /></a>
+    </span>
+    <a v-else :href="href">
+        <slot />
+    </a>
 </template>
 
 <style scoped>
-.Button {
-    display: inline-block;
-    border: 1px solid transparent;
-    text-align: center;
-    font-weight: 600;
-    white-space: nowrap;
+.IconLink {
+    display: inline-flex;
+    align-items: baseline;
+    gap: 0.25em;
 }
 
-.Button:hover {
-    text-decoration: none;
-}
-
-.Button.medium {
-    border-radius: 20px;
-    padding: 0 20px;
-    line-height: 38px;
-    font-size: 14px;
-}
-
-.Button.big {
-    border-radius: 24px;
-    padding: 0 24px;
-    line-height: 46px;
-    font-size: 16px;
-}
-
-.Button.small {
-    border-radius: 16px;
-    padding: 0 16px;
-    line-height: 30px;
-    font-size: 14px;
-}
-
-.Button.brand {
-    border-color: var(--vp-button-brand-border);
-    color: var(--vp-button-brand-text);
-    background-color: var(--vp-button-brand-bg);
-}
-
-.Button.brand:hover {
-    border-color: var(--vp-button-brand-hover-border);
-    color: var(--vp-button-brand-hover-text);
-    background-color: var(--vp-button-brand-hover-bg);
-}
-
-.Button.brand:active {
-    border-color: var(--vp-button-brand-active-border);
-    color: var(--vp-button-brand-active-text);
-    background-color: var(--vp-button-brand-active-bg);
-}
-
-.Button.alt {
-    border-color: var(--vp-button-alt-border);
-    color: var(--vp-button-alt-text);
-    background-color: var(--vp-button-alt-bg);
-}
-
-.Button.alt:hover {
-    border-color: var(--vp-button-alt-hover-border);
-    color: var(--vp-button-alt-hover-text);
-    background-color: var(--vp-button-alt-hover-bg);
-}
-
-.Button.alt:active {
-    border-color: var(--vp-button-alt-active-border);
-    color: var(--vp-button-alt-active-text);
-    background-color: var(--vp-button-alt-active-bg);
-}
-
-.Button.sponsor {
-    border-color: var(--vp-button-sponsor-border);
-    color: var(--vp-button-sponsor-text);
-    background-color: var(--vp-button-sponsor-bg);
-}
-
-.Button.sponsor:hover {
-    border-color: var(--vp-button-sponsor-hover-border);
-    color: var(--vp-button-sponsor-hover-text);
-    background-color: var(--vp-button-sponsor-hover-bg);
-}
-
-.Button.sponsor:active {
-    border-color: var(--vp-button-sponsor-active-border);
-    color: var(--vp-button-sponsor-active-text);
-    background-color: var(--vp-button-sponsor-active-bg);
-}
-</style>
+.GithubIcon {
+    align-self: center;
+}</style>
