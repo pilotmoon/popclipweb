0461304f47e78048cac06918dcde34cfb85cc6f1
Nick Moore <nick@pilotmoon.com>
2023-09-05 11:35:33 +0100

    Improve the appearance of changelogs

--- site/src/Changelog.vue
+++ site/src/Changelog.vue
@@ -3,6 +3,7 @@ import MarkdownIt from 'markdown-it';
 import { computed } from 'vue';
 import { data } from './data/releases.data';
 import { formatDate, formatSize } from './helpers/formatters';
+import { ElTag } from 'element-plus';
 const props = defineProps<{
   channel?: "production" | "beta"
 }>();
@@ -22,32 +23,39 @@ const releases = computed(() => {
 </script>
 
 <template>
-    <div :class="$style.changelog">
-      <div v-for="(release, index) in releases" :key="release.versionString" :class="$style.changelogEntry">
-        <h2>{{ release.versionString }}
-          <span v-if="release.version && !release.versionString.endsWith(String(release.version))"
-            :class="$style.smaller">({{ release.version }})&nbsp;</span>
-          <span :class="$style.smaller"> {{ formatDate(release.date) }}</span>
-        </h2>
-        <span v-if="release.url && release.size && !release.zap && (release.pin || index < 5)">
-          <DownloadButton :href="release.url" size="smaller" theme="outline"  />&nbsp;
-          <span v-if="release.size"><span>{{ formatSize(release.size) }}</span>&nbsp;</span>
-        </span>
-        <span v-if="release.minimumSystemVersion"><span :class="$style.info">≥ macOS {{ release.minimumSystemVersion
-        }}</span>&nbsp;</span>
-        <span v-if="release.archs?.includes('i386')"><span :class="$style.info">Intel 32-bit</span>&nbsp;</span>
-        <span v-if="release.archs?.includes('x86_64')"><span :class="$style.info">Intel 64-bit</span>&nbsp;</span>
-        <span v-if="release.archs?.includes('arm64')"><span :class="$style.info">Apple Silicon</span>&nbsp;</span>
-
-        <div v-html="renderMarkdown(release.description)"></div>
+  <div :class="$style.changelog">
+    <div v-for="(release, index) in releases" :key="release.versionString" :class="$style.changelogEntry">
+      <h2>{{ release.versionString }}
+        <span v-if="release.version && !release.versionString.endsWith(String(release.version))"
+          :class="$style.smaller">({{ release.version }})</span>
+        <span :class="$style.smaller">&ensp;{{ formatDate(release.date) }}</span>
+      </h2>
+      <div :class="$style.info">
+        <DownloadButton v-if="release.url && release.size && !release.zap && (release.pin || index < 5)"
+          :href="release.url" size="smaller" theme="outline" />
+        <ElTag v-if="release.size" type="">{{ formatSize(release.size) }}</ElTag>
+        <ElTag v-if="release.minimumSystemVersion" type="success">≥ macOS {{ release.minimumSystemVersion }}</ElTag>
+        <ElTag v-if="release.archs?.includes('i386')" type="warning">Intel 32-bit</ElTag>
+        <ElTag v-if="release.archs?.includes('x86_64')" type="warning">Intel 64-bit</ElTag>
+        <ElTag v-if="release.archs?.includes('arm64')" type="warning">Apple Silicon</ElTag>
       </div>
+      <div v-html="renderMarkdown(release.description)"></div>
     </div>
-
+  </div>
 </template>
 
 <style module>
 :root {
   --line-height: 1.7;
+}
+
+.info {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 8px;
+
 }
 
 .smaller {
@@ -76,11 +84,4 @@ const releases = computed(() => {
   line-height: var(--line-height);
   font-size: 1em;
 }
-
-.changelog .info {
-  word-wrap: no-wrap;
-  color: var(--vp-c-text-2);
-  background-color: var(--vp-c-bg-soft);
-  padding: 0.1em 0.5em;
-  border-radius: 16px;
-}</style>
+</style>
