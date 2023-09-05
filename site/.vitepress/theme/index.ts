06cf8f4458a8158bcd0af852604e80fd1f93bfcc
Nick Moore <nick@pilotmoon.com>
2023-09-05 01:51:38 +0100

    Add the Setapp banner to the home page

--- site/.vitepress/theme/index.ts
+++ site/.vitepress/theme/index.ts
@@ -4,33 +4,35 @@ import Theme from "vitepress/theme";
 import "./style.css";
 
 // vercel
-import Vercel from '../../src/Vercel.vue';
+import Vercel from "../../src/Vercel.vue";
 
 // local
-import AaButton from '../../src/AaButton.vue';
-import AaLink from '../../src/AaLink.vue';
-import DownloadButton from '../../src/DownloadButton.vue';
-import EditionSwitcher from '../../src/EditionSwitcher.vue';
-import Edition from '../../src/Edition.vue';
+import AaButton from "../../src/AaButton.vue";
+import AaLink from "../../src/AaLink.vue";
+import DownloadButton from "../../src/DownloadButton.vue";
+import EditionSwitcher from "../../src/EditionSwitcher.vue";
+import Edition from "../../src/Edition.vue";
+import HomeFooter from "../../src/HomeFooter.vue";
 
 // element-plus css
 // https://github.com/element-plus/element-plus/blob/dev/packages/theme-chalk/src/common/var.scss
-import 'element-plus/theme-chalk/dark/css-vars.css'
-import 'element-plus/es/components/switch/style/css'
-import 'element-plus/es/components/radio/style/css'
-import 'element-plus/es/components/radio-button/style/css'
-import 'element-plus/es/components/tag/style/css'
-import '../../src/style/overrides.css'
+import "element-plus/theme-chalk/dark/css-vars.css";
+import "element-plus/es/components/switch/style/css";
+import "element-plus/es/components/radio/style/css";
+import "element-plus/es/components/radio-button/style/css";
+import "element-plus/es/components/tag/style/css";
+import "../../src/style/overrides.css";
 
 export default {
   extends: Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'layout-bottom': () => h(Vercel)
+      "layout-bottom": () => h(Vercel),
+      "home-features-after": () => h(HomeFooter),
     });
   },
-  enhanceApp({ app, router, siteData }) {        
+  enhanceApp({ app, router, siteData }) {
     // local
     app.component("AaButton", AaButton);
     app.component("AaLink", AaLink);
---
+++ site/src/HomeFooter.vue
@@ -0,0 +1,34 @@
+<script setup>
+import Setapp from './Setapp.vue'
+</script>
+<template>
+    <div class="container">
+        <div class="HomeFooter">
+            <Setapp />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.HomeFooter {
+    position: relative;
+    padding: 0 24px;
+}
+
+@media (min-width: 640px) {
+    .HomeFooter {
+        padding: 0 48px;
+    }
+}
+
+@media (min-width: 960px) {
+    .HomeFooter {
+        padding: 0 64px;
+    }
+}
+
+.container {
+    margin: 24px auto 0;
+    max-width: 1276px;
+}
+</style>
