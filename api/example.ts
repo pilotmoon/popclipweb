70a50c5f31b247b2235b9bfa547f746b16d5072e
Nick Moore <nick@pilotmoon.com>
2023-10-21 23:12:35 +0100

    edge: adapt cache control

--- api/example.ts
+++ api/example.ts
@@ -4,31 +4,17 @@ export const config = {
  
 export default async function handler(request: Request) {
   const urlParams = new URL(request.url).searchParams;
-  const query = Object.fromEntries(urlParams);
-  const cookies = request.headers.get('cookie');
-  let body;
-  try {
-    body = await request.json();
-  } catch (e) {
-    body = null;
-  }
-
   const iconUrl = new URL("http://api.pilotmoon.com/frontend/icon");
   iconUrl.search = urlParams.toString();
-
-  const res = await fetch(iconUrl.toString());
- 
+  const res = await fetch(iconUrl.toString()); 
   return new Response(
     res.body,
-    // JSON.stringify({
-    //   body,
-    //   query,
-    //   cookies,
-    //   it: iconUrl.toString(),
-    // }),
     {
       status: res.status,
-      headers: res.headers
+      headers: {
+        "Content-Type": res.headers.get("Content-Type") || "text/plain",
+        "Cache-Control": "public, s-maxage=86400, max-age=3600",
+      }
     },
   );
 }
