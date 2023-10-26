32ce7541d6f9d8aaa21df4d16e0a655461504fc3
Nick Moore <nick@pilotmoon.com>
2023-10-26 22:52:41 +0100

    cache L

--- site/src/IconS.vue
+++ site/src/IconS.vue
@@ -28,7 +28,7 @@ const descriptor = computed(() => {
 })
 
 const iconUrl = computed(() => {
-    return apiRoot + "/icon?" + querifyDescriptor(descriptor.value, "2021-10-26-L");
+    return apiRoot + "/icon?" + querifyDescriptor(descriptor.value, "2021-10-26-R");
 });
 
 const src = ref(iconUrl.value);
