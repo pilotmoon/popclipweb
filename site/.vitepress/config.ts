5de0edb6baf20be6cfd1477f26921e46209d3306
Nick Moore <nick@pilotmoon.com>
2023-11-01 05:25:38 +0000

    remove vercel stats; add icons server preconnect

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -192,6 +192,11 @@ export default defineConfig({
     },
   },
   vite: {
+    server: {
+      headers: {
+        'Link': '<https://icons.popclip.app>; rel="preconnect"'
+      },
+    },
     ssr: {
       noExternal: ["element-plus"],
     },
