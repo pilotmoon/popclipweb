3d4d3ecb7a1af727ebe9bf49b5c077884a36d4ee
Nick Moore <nick@pilotmoon.com>
2025-06-27 08:42:59 +0100

    add muted attribute to demo videos

--- site/src/DirectoryPage.vue
+++ site/src/DirectoryPage.vue
@@ -13,15 +13,11 @@ const ext: ExtInfo = {
   ...params.value,
   firstCreated: new Date(params.value?.firstCreated),
   created: new Date(params.value?.created),
-  sourceDate: params.value?.sourceDate
-    ? new Date(params.value?.sourceDate)
-    : null,
-  previousVersions: params.value?.previousVersions.map(
-    (pv: PartialExtInfo) => ({
-      ...pv,
-      sourceDate: pv.sourceDate ? new Date(pv.sourceDate) : null,
-    }),
-  ),
+  sourceDate: params.value?.sourceDate ? new Date(params.value?.sourceDate) : null,
+  previousVersions: params.value?.previousVersions.map((pv: PartialExtInfo) => ({
+    ...pv,
+    sourceDate: pv.sourceDate ? new Date(pv.sourceDate) : null,
+  })),
 } as ExtInfo;
 const slots = useSlots();
 const hasReadme = typeof slots.default?.()?.[0]?.type === "string";
@@ -35,11 +31,7 @@ let cursor: PartialExtInfo = ext;
 for (const ver of ext.previousVersions) {
   // if less than 24 hours before the cursor version, skip
   if (!ver.sourceDate || !cursor.sourceDate) continue;
-  if (
-    ver.sourceDate.getTime() >
-    cursor.sourceDate.getTime() - 24 * 60 * 60 * 1000
-  )
-    continue;
+  if (ver.sourceDate.getTime() > cursor.sourceDate.getTime() - 24 * 60 * 60 * 1000) continue;
   cursor = ver;
   filteredPreviousVersions.push(ver);
 }
@@ -101,20 +93,14 @@ function formatActionTypes(ext: ExtInfo) {
             content="This extension has been vetted and approved by the developer of PopClip."
           >
             <template #reference>
-              <div :class="$style.DownloadInfo">
-                <ShieldTask16Filled /> Verified
-              </div>
+              <div :class="$style.DownloadInfo"><ShieldTask16Filled /> Verified</div>
             </template>
           </ElPopover>
         </ClientOnly>
       </div>
     </div>
 
-    <div
-      v-if="ext.altStrings"
-      :class="$style.AltStrings"
-      style="color: var(--vp-c-text-2)"
-    >
+    <div v-if="ext.altStrings" :class="$style.AltStrings" style="color: var(--vp-c-text-2)">
       <span v-for="alt in ext.altStrings" :key="alt.lang">
         <span :class="$style.Subdued">{{ alt.lang }}</span>
         <b v-if="alt.name">{{ alt.name }}</b>
@@ -125,37 +111,18 @@ function formatActionTypes(ext: ExtInfo) {
 
   <div v-if="ext.popclipVersionIsBeta" class="warning custom-block">
     <p class="custom-block-title">Needs PopClip Beta</p>
-    <p>
-      This extension requires {{ ext.popclipDisplayVersion }} of PopClip,
-      available from <a href="/beta">PopClip Beta</a>.
-    </p>
+    <p>This extension requires {{ ext.popclipDisplayVersion }} of PopClip, available from <a href="/beta">PopClip Beta</a>.</p>
   </div>
   <div v-if="ext.unlisted" class="warning custom-block">
     <p class="custom-block-title">Unlisted Extension</p>
-    <p>
-      This extension is not shown in the directory index. It may be a
-      pre-release or test extension.
-    </p>
+    <p>This extension is not shown in the directory index. It may be a pre-release or test extension.</p>
   </div>
 
   <div v-if="ext.demo" :class="$style.Card">
     <div :class="$style.CardHeader">Demo</div>
     <div :class="$style.Media">
-      <img
-        v-if="ext.demo.endsWith('.gif')"
-        :src="ext.demo"
-        alt="Demo Animated GIF"
-      />
-      <video
-        v-if="ext.demo.endsWith('.mp4')"
-        :src="ext.demo"
-        alt="Demo Video"
-        autoplay
-        loop
-        playsinline
-      >
-        Browser can't show this video.
-      </video>
+      <img v-if="ext.demo.endsWith('.gif')" :src="ext.demo" alt="Demo Animated GIF" />
+      <video v-if="ext.demo.endsWith('.mp4')" :src="ext.demo" alt="Demo Video" autoplay loop playsinline muted>Browser can't show this video.</video>
     </div>
   </div>
 
@@ -168,8 +135,8 @@ function formatActionTypes(ext: ExtInfo) {
   </div>
 
   <!-- <div v-if="ext.actionTypes.includes('javascript') && ext.owner=='github:17520'" :class="$style.Card">
-    <div :class="$style.CardHeader">Say Thanks</div>    
-    
+    <div :class="$style.CardHeader">Say Thanks</div>
+
     <a href="https://buymeacoffee.com/pilotmoon"><img src="/bmac-button.png" width=200 /></a>
   </div> -->
 
@@ -178,40 +145,24 @@ function formatActionTypes(ext: ExtInfo) {
 
     <ul :class="$style.CardData">
       <li v-if="ext.sourceDate">
-        <span :class="$style.CardDataLabel"
-          >{{ ext.previousVersions.length ? "Updated" : "Created" }} </span
-        ><br /><span>{{ formatDate(ext.sourceDate.toISOString()) }}</span>
+        <span :class="$style.CardDataLabel">{{ ext.previousVersions.length ? "Updated" : "Created" }} </span><br /><span>{{
+          formatDate(ext.sourceDate.toISOString())
+        }}</span>
       </li>
       <li>
-        <span :class="$style.CardDataLabel">Version</span><br /><span
-          :title="extractSourceMessage(ext)"
-          >{{ ext.version }}</span
-        >
+        <span :class="$style.CardDataLabel">Version</span><br /><span :title="extractSourceMessage(ext)">{{ ext.version }}</span>
       </li>
       <li>
-        <span :class="$style.CardDataLabel">Identifier</span><br /><code>{{
-          ext.identifier
-        }}</code>
+        <span :class="$style.CardDataLabel">Identifier</span><br /><code>{{ ext.identifier }}</code>
       </li>
-      <li v-if="ext.popclipDisplayVersion">
-        <span :class="$style.CardDataLabel">PopClip Version</span><br />≥{{
-          ext.popclipDisplayVersion
-        }}
-      </li>
-      <li v-if="ext.macosVersion">
-        <span :class="$style.CardDataLabel">macOS Version</span><br />≥{{
-          ext.macosVersion
-        }}
-      </li>
+      <li v-if="ext.popclipDisplayVersion"><span :class="$style.CardDataLabel">PopClip Version</span><br />≥{{ ext.popclipDisplayVersion }}</li>
+      <li v-if="ext.macosVersion"><span :class="$style.CardDataLabel">macOS Version</span><br />≥{{ ext.macosVersion }}</li>
       <li v-if="ext.actionTypes.length">
         <span :class="$style.CardDataLabel">Action Type</span><br />
         {{ formatActionTypes(ext) }}
       </li>
       <li v-if="ext.license">
-        <span :class="$style.CardDataLabel">License</span><br /><a
-          :href="ext.license.url"
-          >{{ ext.license.name }}</a
-        >
+        <span :class="$style.CardDataLabel">License</span><br /><a :href="ext.license.url">{{ ext.license.name }}</a>
       </li>
       <li>
         <span :class="$style.CardDataLabel">Source</span><br />
@@ -228,9 +179,8 @@ function formatActionTypes(ext: ExtInfo) {
       </li>
     </ul>
     <span :class="$style.Small" style="margin-top: 4px"
-      >Third-party product names and logos are used solely to identify
-      compatible apps, websites, or services, and do not imply endorsement by
-      the respective entities.
+      >Third-party product names and logos are used solely to identify compatible apps, websites, or services, and do not imply endorsement by the respective
+      entities.
     </span>
   </div>
 
@@ -241,9 +191,7 @@ function formatActionTypes(ext: ExtInfo) {
         <span :title="extractSourceMessage(ver)"
           >Version {{ ver.version }}
           <span :class="$style.small"
-            >({{ formatDate(ver.sourceDate)
-            }}<span v-if="ver.name !== ext.name">, as "{{ ver.name }}"</span
-            >)</span
+            >({{ formatDate(ver.sourceDate) }}<span v-if="ver.name !== ext.name">, as "{{ ver.name }}"</span>)</span
           >:</span
         >
         <a v-if="ver.source" :href="ver.source">Source</a>,
