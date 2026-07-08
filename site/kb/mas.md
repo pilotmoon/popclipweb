38fe4fe56dbfd9f8b99ddd8e57ff4019ee57b4f5
Nick Moore <nick@pilotmoon.com>
2026-07-08 10:04:07 +0100

    not "for free"

--- site/kb/mas.md
+++ site/kb/mas.md
@@ -6,8 +6,8 @@ import PopClipVersion from "../src/PopClipVersion.vue";
 
 The latest version of PopClip is <PopClipVersion />, but it is not available as
 an update on the Mac App Store. If you are still using the last released Mac App
-Store version, 2023.9, then you can migrate for free to the Standalone edition
-to receive the latest updates.
+Store version, 2023.9, then you can migrate to the Standalone edition to receive
+the latest updates.
 
 ::: info From the developer
 
--- site/src/InfoBox.vue
+++ site/src/InfoBox.vue
@@ -34,7 +34,7 @@ const shouldShowInfoBox = true;
     </p>
     <p>
       The latest version of PopClip is <PopClipVersion />, but it is not available as an update on the Mac App Store. If you are still using the last released
-      Mac App Store version, 2023.9, then you can <a href="/kb/mas">migrate for free to the Standalone edition</a> to receive the latest updates.
+      Mac App Store version, 2023.9, then you can <a href="/kb/mas">migrate to the Standalone edition</a> to receive the latest updates.
     </p>
   </div>
 </template>
